2ba92731669241128ace845fc07d913ab4a2eb2c
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-09-29 11:40:08 -0300

    refatoring subcategory loop block

--- custom/Cart/category_spec.rb
+++ custom/Cart/category_spec.rb
@@ -6,9 +6,9 @@ require 'pry'
 RSpec::Matchers.define :contain_products do |*products|
 	match do |category|
     # subcategories_products = category.subcategories.flat_map(&:product)
-    @instances = []
+    subcategories_products = category.subcategories.product.flat_map(&:name)
     category.subcategories.each{ |instance| instance.instance_of?(category.class) ? @instances.push(instance) : @instances  }
-    binding.pry
+    
     # Quebra por conta de pry(#<RSpec::Matchers::DSL::Matcher>)> category.subcategories.each { |instance| p instance_of?(Subcategory) }
 		expect(subcategories_products & products).to eq products
 	end
