a1a9322d82dbfa473e0bd85ca13d779fc5dc2c82
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-10-02 11:49:48 -0300

    add behaviour spec

--- spec/behaviour_spec.rb
+++ spec/behaviour_spec.rb
@@ -1,6 +1,13 @@
 RSpec.describe DeepThought do
   it "prints the answear to the ultimate question" do
     printer = double("printer")
-    computer = DeepThought.new(printer)   
+    computer = DeepThought.new(printer)
+
+    # programa o objeto printer para verificar se o método print
+    # será chamado nesse objeto com o argumento "42" durante o
+    # teste
+    expect(printer).to receive(:print).with("42")
+
+    computer.print_the_answer
   end
 end
